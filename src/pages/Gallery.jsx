572a387db979ddf435d428700af1f1e9aaab8be1
mjlnavarro <mjlnavarro@tip.edu.ph>
2025-06-30 13:47:07 +0800

    gallery.jsx changes

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -7,10 +7,10 @@ function Gallery() {
       <h3>Certificates & Gallery</h3>
       <GalleryLightbox
         images={[
-          "./dist/public/images/jin1.jpg",
-          "./dist/public/images/jin2.jpg",
-          "./dist/public/images/jin3.jpg",
-          "./dist/public/images/jin4.jpg",
+          "../public/images/jin1.jpg",
+          "../public/images/jin2.jpg",
+          "../public/images/jin3.jpg",
+          "../public/images/jin4.jpg",
         ]}
       />
     </section>
